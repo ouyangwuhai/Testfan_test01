f1d3089409539e27f8133e91842588a75941a32b
ouyangwuhai <303423249@qq.com>
2019-01-12 22:53:37 +0800

    修改测试报告路径

--- runcases.py
+++ runcases.py
@@ -3,10 +3,12 @@
 import unittest
 import HTMLTestRunner
 import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.header import Header
+from email.mime.application import MIMEApplication
 
 #导入案例文件时，如果提示红线，则设置存放案例文件的文件夹mytestcase文件夹上一层目录pyunittest
 #右击Mark Directory as ...  -> Source root则会
@@ -28,29 +30,25 @@ username = '303423249@qq.com'     #发件人邮箱地址
 password = 'qthyfnszdkesbgfb'     #密码是邮箱使用客户端的授权码
 sender = '303423249@qq.com'       #发件人邮箱地址
 receiver = ['303423249@qq.com']   #收件人邮箱地址
-subject = 'Python email test'     #邮件主题
+subject = 'runcases_result test report'     #邮件主题
 mailbody = "Hi!\nHow are you?\nHere is your report" #邮件正文
-attachfile = reportname             #邮件附件
+report_dir = report_dir = os.path.abspath('.') + "\\runcases_result.html"
 
 #构造邮件内容
-msg = MIMEMultipart('mixed')     #创建MIMEMultipart邮件对象，类型为mixed
+msg = MIMEMultipart()     #创建MIMEMultipart邮件对象，类型为mixed
 msg['Subject'] = subject         #主题赋值
 msg['From'] = '303423249@qq.com <303423249@qq.com>' #发件人赋值
-# 收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
-msg['To'] = ";".join(receiver)   #收件人赋值
+msg['To'] = ";".join(receiver)   #收件人赋值# 收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
 
 # 构造文字内容
-text_plain = MIMEText(mailbody, 'plain', 'utf-8')
+text_plain = MIMEText(mailbody, 'html', 'utf-8')
 msg.attach(text_plain)
 
 # 构造附件
-sendfile = open(attachfile, 'rb').read()
-text_att = MIMEText(sendfile, 'base64', 'utf-8')
-text_att["Content-Type"] = 'application/octet-stream'
-# text_att["Content-Disposition"] = 'attachment; filename="smail.py"'
-# 另一种实现方式
-text_att.add_header('Content-Disposition', 'attachment', filename=attachfile)
-msg.attach(text_att)
+att = MIMEApplication(open(report_dir,'rb').read())
+att["Content-Type"] = 'application/octet-stream'
+att.add_header('Content-Disposition', 'attachment', filename= report_dir)
+msg.attach(att)
 
 # 发送邮件
 smtp = smtplib.SMTP()
